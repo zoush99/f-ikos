24a1bfee532323f971367436a96187d43de44af6
zoush99 <zoush99@gmail.com>
2024-04-02 22:14:43 +0800

    Before modify floating point array

--- benchmark/test/test.c
+++ benchmark/test/test.c
@@ -1,9 +1,12 @@
+
+extern void __ikos_assert(int);
  int main() {
      int x=3;
      int y=3;
      int i=0;
      int a=1;
      int b =1;
+     __ikos_assert(b!=1);
      x = x / (y-x);
      return x;
  }
